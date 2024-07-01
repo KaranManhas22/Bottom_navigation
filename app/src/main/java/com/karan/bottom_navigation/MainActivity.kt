dbed02e8f1c2185172929fbfb71bfbce1ab6e445
Karan Singh Manhas <karansingh33477@gmail.com>
2024-07-01 16:45:37 +0530

    Initial commit

---
+++ app/src/main/java/com/karan/bottom_navigation/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.karan.bottom_navigation
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.replace
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.karan.bottom_navigation.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+    lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        navController = findNavController(R.id.navController)
+
+        navController.addOnDestinationChangedListener { navController, destination, argument ->
+
+        }
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.first -> navController.navigate(R.id.fragement1)
+                R.id.second -> navController.navigate(R.id.fragment2)
+                R.id.third -> navController.navigate(R.id.fragment3)
+
+            }
+            return@setOnItemSelectedListener true
+        }
+    }
+}
+
